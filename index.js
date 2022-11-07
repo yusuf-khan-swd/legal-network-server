a1026fc176926cfd2c0508798da0cc2aa613788e
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-08 02:37:09 +0600

    create jwt token

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+app.get('/jwt', (req, res) => {
+  const email = req.query;
+  const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+  res.send({ token });
+});
 
 app.get('/', (req, res) => {
   res.send('Legal Network Server Is Running.');
