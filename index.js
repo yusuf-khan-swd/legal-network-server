01c7bda029aa1520ecfb551636a6620c51f5bf80
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-09 20:00:23 +0600

    get a single review from db by id

--- index.js
+++ index.js
@@ -112,6 +112,14 @@ async function run() {
       const result = await reviewsCollection.deleteOne(query);
       res.send(result);
     });
+
+    app.get('/my-review/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewsCollection.findOne(query);
+      res.send(result);
+    });
+
   }
   finally {
 
