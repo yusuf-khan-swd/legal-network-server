783b316bb1750fef3270cbb0a0a62faa5b590e72
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-09 23:00:17 +0600

    update review by using put

--- index.js
+++ index.js
@@ -120,6 +120,24 @@ async function run() {
       res.send(result);
     });
 
+    app.put('/my-review/:id', async (req, res) => {
+      const id = req.params.id;
+      const { title, description } = req.body;
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          title: title,
+          description: description
+        }
+      };
+
+      const result = await reviewsCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+
+    });
+
+
   }
   finally {
 
